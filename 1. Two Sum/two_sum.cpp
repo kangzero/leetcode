9771d379ef66eeceecd08982e308333c8dc62b80
nkang <kangzero@gmail.com>
2019-03-23 18:06:40 -0700

    Update two_sum.cpp
    add algorithm of sort + greedy

--- 1. Two Sum/two_sum.cpp
+++ 1. Two Sum/two_sum.cpp
@@ -1,45 +1,70 @@
 //#define BRUTE_FORCE
-#define HASH_MAP
-
-#ifdef BRUTE_FORCE
+//#define HASH_MAP
+#define SORT_GREEDY
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {    
+#ifdef BRUTE_FORCE
         int i, j = 0;
-        vector<int> a(2, 0);
+        vector<int> ret(2, -1);
         
         for (i = 0; i < nums.size(); i++) {
             for (j = i + 1; j < nums.size(); j++) {
                 if (nums[j] == (target - nums[i]) ) {
-                    a[0] = i;
-                    a[1] = j;
-                    return a;
+                    ret[0] = i;
+                    ret[1] = j;
+                    return ret;
                 }
             }
         }
-        
-        return {-1, -1};
-    }
-};
-
+        return ret;
 #elif defined HASH_MAP
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
+        vector<int> res(2, -1);
         
         for(int i=0; i<nums.size(); i++) {
             auto p = map.find(target - nums[i]);
             if (p != map.end()) {
-                return {p->second, i};
+                res[0] = p->second;
+                res[1] = i;
+                return res;
             }
             map[nums[i]] = i;
         }
+        return res;     
+#elif defined SORT_GREEDY
+        vector<int> v;
+        for (int i = 0; i < nums.size(); i++) {
+            v.push_back(nums[i]);
+            cout << v[i] << " ";
+        }
+        cout << endl;
         
-        return {-1, -1};
+        sort(v.begin(), v.end());
+        int l = 0;
+        int r = v.size() - 1;
+        int p = 0, q = 0;
+        while (l < r) {
+            if ((v[l] + v[r]) > target) {
+                r--;
+            } else if ((v[l] + v[r]) < target) {
+                l++;
+            } else {
+                cout << l << " " << r << endl;
+                p = v[l];
+                q = v[r];
+                break;
+            }
+        }
+        
+        vector<int> res;
+        for (int i = 0; (i < nums.size()) & (res.size() < 2); i++) {
+            if ((nums[i] == p) || (nums[i] == q)) {
+                res.push_back(i);
+            }
+        }
+        return res;
+#endif    
     }
 };
-
-#endif
