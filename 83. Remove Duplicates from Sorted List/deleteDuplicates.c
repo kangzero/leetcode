7b22bb92906ab460fad29e5bb414db0f981a324e
nkang1221 <kangzero@gmail.com>
2019-04-29 17:59:30 -0700

    Update deleteDuplicates.c

--- 83. Remove Duplicates from Sorted List/deleteDuplicates.c
+++ 83. Remove Duplicates from Sorted List/deleteDuplicates.c
@@ -9,8 +9,9 @@
 
 struct ListNode* deleteDuplicates(struct ListNode* head)
 {
+    if ((head == NULL) || (head->next == NULL)) return head;
+        
     struct ListNode *cur = head;
-    
     while ((cur != NULL) && (cur->next != NULL)) 
     {
         if (cur->val == cur->next->val) 
