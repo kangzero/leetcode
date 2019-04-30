8aa03bd3fbc4a21ab0e0cbfc2041581c3639a139
nkang1221 <kangzero@gmail.com>
2019-04-29 23:25:27 -0700

    Create getIntersectionNode.cpp

---
+++ 160. Intersection of Two Linked Lists/getIntersectionNode.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p1 = headA;
+        ListNode *p2 = headB;
+        
+        while (p1 != p2) {
+            p1 = (p1 != nullptr) ? p1->next : headB;
+            p2 = (p2 != nullptr) ? p2->next : headA;
+        }
+        
+        return p1;
+    }
+};
