146cb079bbee9cd6cc92151d660ec742ed6696a1
nkang1221 <kangzero@gmail.com>
2019-05-13 22:43:54 -0700

    Create isToeplitzMatrix.c

---
+++ 766. Toeplitz Matrix/isToeplitzMatrix.c
@@ -0,0 +1,9 @@
+bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize){    
+    for (int i = 0; i < matrixSize - 1; i++) {
+        for (int j = 0; j < matrixColSize[i] - 1; j++) {
+            if (matrix[i][j] != matrix[i+1][j+1]) return false;
+        }
+    }
+    
+    return true;
+}
