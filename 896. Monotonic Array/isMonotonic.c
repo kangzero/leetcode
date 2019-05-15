f855a99001abc4420936f7ae8d0367b2fd5f20fd
nkang1221 <kangzero@gmail.com>
2019-05-14 23:56:49 -0700

    Create isMonotonic.c

---
+++ 896. Monotonic Array/isMonotonic.c
@@ -0,0 +1,19 @@
+bool isMonotonic(int* A, int ASize) 
+{
+    bool increasing = true;
+    bool decreasing = true;
+    
+    for (int i = 0; i < (ASize-1); i++) 
+    {
+        if (A[i] > A[i+1])
+        {
+            increasing = false;
+        }
+        if (A[i] < A[i+1])
+        {
+            decreasing = false;
+        }
+    }
+    
+    return (increasing || decreasing);
+}
