80e53e53429d94478807b0cafe945869af7f656a
nkang1221 <kangzero@gmail.com>
2019-05-29 19:22:28 -0700

    Create getRow.cpp

---
+++ 119. Pascal's Triangle II/getRow.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> ret((rowIndex+1), 0);
+        
+        ret[0] = 1;
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i; j >= 1; j--) {
+                ret[j] += ret[j-1];
+            }
+        }
+        
+        return ret;
+    }
+};
